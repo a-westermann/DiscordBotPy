1c96a8dd96d2659427a5792278d39c758a7d746d
Vierce1 <a.westermann.19@gmail.com>
2023-05-04 12:24:29 -0500

    testing writing names

--- Commands.py
+++ Commands.py
@@ -87,7 +87,8 @@ class OtherCommands(app_commands.Group):
             #     names_list_file.write(f"{name}\n")
             # finally, add the new name to the bottom of the used_names file
             used_names_file = open("/home/andweste/Scripts/used_names.txt", "r") # open in read, then overwrite all
-            used_names_text = used_names_file.readlines()
+            used_names_text = used_names_file.read()
+            used_names_text = used_names_text.replace(last_date, today)
             for x in used_names_file:
                 print(x)
             used_names_file = open("/home/andweste/Scripts/used_names.txt", "w")

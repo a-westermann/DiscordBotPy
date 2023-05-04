a95c905fd4a779ae6e3a834b4e3fa05aa8b69085
Vierce1 <a.westermann.19@gmail.com>
2023-05-04 14:34:27 -0500

    command grouping

--- helpers.py
+++ helpers.py
@@ -3,6 +3,6 @@ from googlesearch import search
 
 
 # General functions
-def google_search(self, search_term, num_results):
+def google_search(search_term, num_results):
     for url in search(search_term, num_results=num_results):
         return url

835b11de1bf5b51101e1e2895343ef06dee3de3c
Vierce1 <a.westermann.19@gmail.com>
2023-05-04 14:03:47 -0500

    command grouping

--- Commands.py
+++ Commands.py
@@ -17,11 +17,11 @@ class Commands(app_commands.Group):
         self.tree = app_commands.CommandTree(client)
         return self.tree
 
-    @app_commands.command.group(name='other', invoke_without_command=True)
+    @bot.group(name='other', invoke_without_command=True)
     async def other(self, interaction:discord.Interaction):
         pass
 
-    @commands.command.group(name='lol', invoke_without_command=True)
+    @bot.group(name='lol', invoke_without_command=True)
     async def lol(self, interaction:discord.Interaction):
         pass
 

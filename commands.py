66a74186052c175f8f763a4399e6306903bb1fe8
Vierce1 <a.westermann.19@gmail.com>
2023-05-05 10:40:21 -0500

    Update to 3 names a day

--- commands.py
+++ commands.py
@@ -1,3 +1,5 @@
+import datetime
+import LeagueModels.league_api
 import discord
 from discord.ext import commands
 from discord import app_commands
@@ -8,23 +10,24 @@ from LeagueModels import league_api
 import Baby.baby
 
 
+
 # League
 class Lol(app_commands.Group):
     def __init__(self, discord_bot: discord.ext.commands.Bot):
         super().__init__()
-        global bot
-        bot = discord_bot
+        self.league_api = LeagueModels.league_api.LeagueAPI
+        self.bot = discord_bot
+
+    @app_commands.command(name="test")
+    async def test(self, interaction: discord.Interaction):
+        summoner = self.league_api.LeagueAPI.get_summoner("Vierce")
+        await interaction.response.send_message()
+
 
 
     @app_commands.command(name="recap", description="Get a recap of your history with a champ")
     async def recap(self, interaction: discord.Interaction):
         await interaction.response.send_message("in development")
-
-    @app_commands.command(name="test")
-    async def test(self, interaction: discord.Interaction):
-        summoner = league_api.LeagueAPI.get_summoner("Vierce")
-        await interaction.response.send_message()
-
 
 
 
@@ -35,11 +38,12 @@ class OtherCommands(app_commands.Group):
     def __init__(self, bot: discord.ext.commands.Bot):
         super().__init__()
         self.bot = bot
+        self.baby = Baby.baby.BabyStuff
 
 
-    @app_commands.command(name="hello")
+    @app_commands.command(name="ping")
     async def say_hello(self, interaction: discord.Interaction):
-        print('saying hello')
+        print('pinging server')
         await interaction.response.send_message("hello")
 
 
@@ -69,7 +73,7 @@ class OtherCommands(app_commands.Group):
 
     @app_commands.command(name="todays_baby_name", description="gives today's baby name")
     async def todays_baby_name(self, interaction: discord.Interaction):
-        name = Baby.baby.BabyStuff.get_todays_name()
+        name = baby.get_todays_name()
         search_results = helpers.google_search(search_term= name + " girl's name origin", num_results=1)
         await interaction.response.send_message("Name: " + name + search_results)
 

64e8ba306b5bbdec07a32e1f721f19842d05105f
Vierce1 <a.westermann.19@gmail.com>
2023-05-06 19:47:09 -0500

    Update to 3 names a day

--- Baby/baby_view.py
+++ Baby/baby_view.py
@@ -20,3 +20,51 @@ class BabyView(discord.ui.View):
         self.score = 2
         print("scored 2")
         self.stop()
+
+    @discord.ui.button(label="3", style=discord.ButtonStyle.red)
+    async def three(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.score = 3
+        print("scored 3")
+        self.stop()
+
+    @discord.ui.button(label="4", style=discord.Button.style.blurple)
+    async def four(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.score = 4
+        print("scored 4")
+        self.stop()
+
+    @discord.ui.button(label="5", style=discord.ButtonStyle.blurple)
+    async def five(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.score = 5
+        print("scored 5")
+        self.stop()
+
+    @discord.ui.button(label="6", style=discord.ButtonStyle.blurple)
+    async def six(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.score = 6
+        print("scored 6")
+        self.stop()
+
+    @discord.ui.button(label="7", style=discord.ButtonStyle.green)
+    async def seven(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.score = 7
+        print("scored 7")
+        self.stop()
+
+    @discord.ui.button(label="8", style=discord.ButtonStyle.green)
+    async def eight(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.score = 8
+        print("scored 8")
+        self.stop()
+
+    @discord.ui.button(label="9", style=discord.ButtonStyle.green)
+    async def nine(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.score = 9
+        print("scored 9")
+        self.stop()
+
+    @discord.ui.button(label="10", style=discord.ButtonStyle.green)
+    async def ten(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.score = 10
+        print("scored 10")
+        self.stop()

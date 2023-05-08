f881ac1e45bfffe58a53bc3f3ac91b088cb45b08
Vierce1 <a.westermann.19@gmail.com>
2023-05-08 14:51:10 -0500

    authorized users on baby

--- commands.py
+++ commands.py
@@ -107,7 +107,7 @@ class OtherCommands(app_commands.Group):
 
     @app_commands.command(name="baby_name_summary", description="get the top rated names")
     async def baby_name_summary(self, interaction: discord.Interaction):
-        embed = discord.Embed(colour=discord.Colour.dark_gold, description="This is a test embed")
+        embed = discord.Embed(color=discord.Color.from_rgb(1, 1, 1), description="This is a test embed")
         await interaction.response.send_message(embed=embed)
 
 

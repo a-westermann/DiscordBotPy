1579f23a57f6fb9a0b1c38c4d71f78c69caf98d1
Vierce1 <a.westermann.19@gmail.com>
2023-05-09 11:11:31 -0500

    authorized users on baby

--- commands.py
+++ commands.py
@@ -117,5 +117,9 @@ class OtherCommands(app_commands.Group):
         ashley_names = set(['Evelyn', 'Heidi', 'Paytona500', 'Sophia'])
         andrew_names = set(['Evelyn', 'Heidi', 'Rosemary', 'Anya'])
         venn = venn3([ashley_names, andrew_names], ('Ashley_Names', 'Andrew_Names'))
-        pyplot.show()
+        # pyplot.show()
+        chart_file = "names_diagram.png"
+        pyplot.savefig(chart_file)
+        chart_image = discord.File(chart_file)
+        await interaction.response.send_message(file=chart_image)
 

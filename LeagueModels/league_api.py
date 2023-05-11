c1d78d8698c28733ab74deb0c120b17589b8ed46
Vierce1 <a.westermann.19@gmail.com>
2023-05-11 14:00:16 -0500

    baby name diagram completed

--- LeagueModels/league_api.py
+++ LeagueModels/league_api.py
@@ -8,7 +8,7 @@ class LeagueAPI:
         global token
         token = api_token
         global lol_watcher
-        lol_watcher = LolWatcher(token)
+        lol_watcher = LolWatcher(api_key=token)
 
     def get_summoner(self, summoner_name):
         return lol_watcher.summoner.by_name(region, summoner_name)

eb64cd2aedd45fd6119b42770d1bccd31a187402
Vierce1 <a.westermann.19@gmail.com>
2023-05-11 13:02:29 -0500

    authorized users on baby

--- Baby/baby_chart.py
+++ Baby/baby_chart.py
@@ -10,19 +10,16 @@ def get_baby_venn():
     subsets = (1, 1, 1)
     ashley_names = set(['Evelyn', 'Heidi', 'Paytona500', 'Sophia'])
     andrew_names = set(['Evelyn', 'Heidi', 'Rosemary', 'Anya'])
-    venn = venn2(subsets=subsets, set_labels=('Andrew', 'Ashley', 'Shared'))
+    venn = venn2(subsets=(ashley_names, andrew_names), set_labels=('Andrew', 'Ashley', 'Shared'))
 
     # labels =  ['Ashley_Names', 'Andrew_Names']
     # labels = ['Andrew', 'Ashley', 'Shared']
     # for label in labels:
     #     venn.get_label_by_id(label).set_text(label)
     # venn.get_label_by_id('100').set_text('andrew')
-    # pyplot.show()
+
     pyplot.title("Baby Names")
-    # pyplot.show()
     chart_file = "names_diagram.png"
-    # pyplot.figure()
-    pyplot.savefig(chart_file)
+    pyplot.savefig(chart_file) # could pass in dpi to savefig as chart's dpi to increase resolution
     chart_image = discord.File(chart_file)
-    # could pass in dpi to savefig as chart's dpi to increase resolution
     return chart_image

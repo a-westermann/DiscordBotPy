248e031d09a800aac36e1dd35ca12961eecf9904
Vierce1 <a.westermann.19@gmail.com>
2023-05-19 13:45:50 -0500

    league api start

---
+++ psql.py
@@ -0,0 +1,14 @@
+import psycopg2
+
+
+class PSQL:
+    def __init__(self, database: str, username: str, password: str, hostname: str, port: int):
+        self.connection = psycopg2.connect(database=database, username=username, password=password,
+                                      hostname=hostname, port=port)
+        self.cursor = self.connection.cursor()
+
+
+    def get_summoner_matches(self, summoner_name):
+        self.cursor.execute("SELECT * FROM match_history")
+        records = cursor.fetchall()
+        print(records)

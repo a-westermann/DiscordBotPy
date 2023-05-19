6bb1ffb8265579deb7090af49efc5bf3ecc51a6b
Vierce1 <a.westermann.19@gmail.com>
2023-05-19 15:29:46 -0500

    league api start

--- helpers.py
+++ helpers.py
@@ -64,6 +64,6 @@ def get_used_babies(user: str, top: bool, number: int, include_score: bool):
 def get_summoner_name_from_first_letter(letter: str):
     if letter.lower() == "v": return "Vierce"
     if letter.lower() == "t": return "The Great Ratsby"
-    if letter.lower() == "c": return "CommradeGiraffe"
+    if letter.lower() == "c": return "ComradeGiraffe"
     if letter.lower() == "g": return "Gold Force"
 

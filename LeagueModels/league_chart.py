7f3b61e414500dc7345bdc7d538fe5e37cf8adc6
Vierce1 <a.westermann.19@gmail.com>
2023-05-19 16:05:15 -0500

    league api start

--- LeagueModels/league_chart.py
+++ LeagueModels/league_chart.py
@@ -7,6 +7,15 @@ import helpers
 
 
 def plot_kda(sql_match_rows):
-    for match in sql_match_rows:
-        print("kills = " + str(match["kills"]))
-        break
+    kda_points = []
+    for i, match in enumerate(sql_match_rows):
+        # for each match, look at the last 10 and create the kda average
+        for j in range(10):
+            if j > i:
+                break  # ensures for the first 10 games in the list we don't try to go negative i
+            evaulate_match = sql_match_rows[i - j]
+            kda = (evaulate_match["kills"] + evaulate_match["assists"]) + evaulate_match["deaths"]
+            kda_points.append(round(kda, 2))
+
+    for kda in kda_points:
+        print(kda)

11fca9a72f39ca2bb48c627c49b6567ff1106133
Vierce1 <a.westermann.19@gmail.com>
2023-05-19 16:26:36 -0500

    league api start

--- LeagueModels/league_chart.py
+++ LeagueModels/league_chart.py
@@ -22,7 +22,7 @@ def plot_kda(sql_match_rows):
         deaths = deaths if deaths > 0 else 1
         kda = (kills + assists) / deaths
         match_date = str(evaulate_match["date_created"])
-        kda_points.append((round(kda, 2), match_date))
+        kda_points.append(tuple(round(kda, 2), match_date))
 
     x = kda_points[1]
     y = kda_points[0]

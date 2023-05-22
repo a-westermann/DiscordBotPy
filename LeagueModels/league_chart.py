57cae9c356dbfb939716b1a5c8ecf8643fa4f930
Vierce1 <a.westermann.19@gmail.com>
2023-05-22 12:49:56 -0500

    group chart y-axis

--- LeagueModels/league_chart.py
+++ LeagueModels/league_chart.py
@@ -64,7 +64,6 @@ def group_plot_kda(sql_match_rows, summoners):
     # datetime.datetime.strptime(last_date, '%Y-%m-%d')
     # date_range = [first_date, last_date]
     match_dates = set()
-    match_dates_lists = [[] for _ in range(len(summoners))]
     # for s in summoners:
     #     kda_points.append([])
     for i, match in enumerate(sql_match_rows):
@@ -84,34 +83,32 @@ def group_plot_kda(sql_match_rows, summoners):
         deaths = deaths if deaths > 0 else 1
         kda = (kills + assists) / deaths
         list_index = list(summoners).index(summoner)
-        kda_points[list_index].append(round(kda, 2))
+        kda_points[list_index].append((match_date, round(kda, 2)))
         match_date = str(evaulate_match["date_created"]).split(' ')[0]
         match_date = datetime.datetime.strptime(match_date, '%Y-%m-%d')
         match_dates.add(match_date)
-        # also add it to the list so we can x-ref with the kda's when building y-axis below
-        match_dates_lists[list_index].append(match_date)
 
 
     x = list(match_dates)
     x.sort()
     y_values = []
-    print(str(match_dates_lists[0][0]).split(' ')[0])
     dates_list = [str(date.date()) for date in x]  # get a list of the dates in the set
-    print(dates_list[0])
     for i, kda_list in enumerate(kda_points):  # add the kda_list for each summoner to the y_values list
         # y = np.full(len(x), np.nan)  # np.nan fill in values = to # of x values. We will replace them w/ Y values
         # create a mask instead to fill in missing points
         mask = np.ones(len(x), dtype=bool)
         # iterate through dates AND the kda match history for this summoner & fill in matches that match the date
         for j, date in enumerate(dates_list):
-            for k, match_d in enumerate(match_dates_lists[i]):
-                match_d = str(match_d).split(' ')[0]
-                if date == match_d:
+            for k, m_date in enumerate(kda_list[0].reverse()):
+                # reverse it so latest game played on that date is first for the match
+                m_date = str(m_date).split(' ')[0]
+                if date == m_date:
                     mask[j] = False # turn off mask, found match for this point
                     # y[j] = kda_list[k]
                     # y[j] = kda_list.pop()
-        y = np.array(kda_list)
+        y = np.array(kda_list[1])
         y = np.insert(y, 0, np.nan)
+        # I need to reduce the length of y to be equal to x, doesn't allow mismatch axis lengths
         y = np.insert(y, len(x), np.nan)
         # fill in missing values with the mask
         y = np.ma.array(y, mask=mask)

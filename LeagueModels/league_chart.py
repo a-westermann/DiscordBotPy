51197dc69ba48c054afc10ea54e8330929531010
Vierce1 <a.westermann.19@gmail.com>
2023-05-22 15:29:58 -0500

    group chart y-axis

--- LeagueModels/league_chart.py
+++ LeagueModels/league_chart.py
@@ -67,6 +67,8 @@ def group_plot_kda(sql_match_rows, summoners):
                 summoner_match_rows[i].append(match)
 
 
+    for m in summoner_match_rows[1]:
+        print(m["summoner_name"] + "  -   " + str(m["kills"]) + str(m["deaths"]) + str(m["assists"]))
 
     # now iterate through each summoner's table, and each match inside it to build the kda
     for match_table in summoner_match_rows:

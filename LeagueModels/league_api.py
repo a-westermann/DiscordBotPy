12ec6d53efdc5139144dfbcedbfa068dac3d715b
Vierce1 <a.westermann.19@gmail.com>
2023-05-31 14:55:03 -0500

    module requirements

--- LeagueModels/league_api.py
+++ LeagueModels/league_api.py
@@ -43,7 +43,10 @@ class LeagueAPI:
         return items.find(item=item_to_find)
 
     def get_champ(self, champ_id: int):
-        return cass.get_champion(key=champ_id, region=cass.Region.north_america)
+        champs = cass.get_champion(region=cass.Region.north_america)
+        return cass.Champions.find(id=champ_id)
+
+        # return cass.get_champion(key=champ_id, region=cass.Region.north_america)
 
 
 

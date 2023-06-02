a6161a48c23fbd2bfdb06f8687a5e49b30f8946e
Vierce1 <a.westermann.19@gmail.com>
2023-06-02 14:49:43 -0500

    recap function

--- program.py
+++ program.py
@@ -22,6 +22,7 @@ def create_client():
 def get_token(in_rasp_pi):
      path_to_token = "H:\\Coding\Python Projects\DiscordBot\\token.txt" if in_rasp_pi == False \
          else "/home/andweste/Scripts/token.txt"
+     print("token =  " + open(path_to_token).read())
      return open(path_to_token).read()
 
 

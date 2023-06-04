192a50bb0a2c4da91b0246146987f3c7e57499f5
Vierce1 <a.westermann.19@gmail.com>
2023-06-04 15:32:20 -0500

    back-fill match function

--- LeagueModels/league_api.py
+++ LeagueModels/league_api.py
@@ -138,7 +138,7 @@ class LeagueAPI:
         description_string += f"\n\n**Best game** ({best_game_date}):\n" \
             f"KDA:  **{kda}** ({kills}/{deaths}/{assists})\n" \
             f"{multi_kills}\n" \
-            f"'''cs\n    {items[0]}  \n    {items[1]}  \n    {items[2]}  \n" \
+            f"'''css\n    {items[0]}  \n    {items[1]}  \n    {items[2]}  \n" \
             f"    {items[3]}  \n    {items[4]}  \n    {items[5]}\n'''"
 #TODO: Add Win rate (would need to update the table. What else?
         embed.add_field(name='', value=description_string, inline=False) # inline only affects Name, so doesn't work

a0cb0a8aaa7c3af1c123b113fe53794875dad28f
Vierce1 <a.westermann.19@gmail.com>
2023-06-05 12:28:56 -0500

    updates to recap

--- LeagueModels/league_api.py
+++ LeagueModels/league_api.py
@@ -130,7 +130,8 @@ class LeagueAPI:
                 items.append("")
             else:
                 item = self.get_item(item_id)
-                items.append(f"**{item.name}** *{item.description}*")
+                items.append(f"**{item.name}** *{item.name}*")
+                print(item.description)
         description_string += f"\n\n**Best game** ({best_game_date}):\n" \
             f"KDA:  **{kda}** ({kills}/{deaths}/{assists})\n" \
             f"{multi_kills}\n" \

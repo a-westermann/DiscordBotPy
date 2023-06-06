ada7ae1c0908e88b26d148ef651fc47849e92da5
Vierce1 <a.westermann.19@gmail.com>
2023-06-06 14:51:02 -0500

    updates to recap

--- LeagueModels/league_api.py
+++ LeagueModels/league_api.py
@@ -137,6 +137,8 @@ class LeagueAPI:
                 items.append("")
             else:
                 item = self.get_item(item_id)
+                item_data = cass.get_item_data()
+                print(item.keywords)
                 tags = f"{item.tags[0]} / {item.tags[1]}" if len(item.tags) > 1 else f"{item.tags[0]}"
                 items.append(f"**{item.name}** ---> *{tags}*")
         description_string += f"\n\n**Best game** ({best_game_date}):\n" \

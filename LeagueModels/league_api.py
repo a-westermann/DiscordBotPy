5200f285759cbf33ec78e09150fd5d853baedbc0
Vierce1 <a.westermann.19@gmail.com>
2023-06-08 10:02:22 -0500

    updates to recap

--- LeagueModels/league_api.py
+++ LeagueModels/league_api.py
@@ -143,7 +143,9 @@ class LeagueAPI:
         # sort items putting Mythic at top and adding a blank line after
         sorted_items = []
         for item in items:
-            sorted_items.append(f"{item}\n" if 'Mythic' in item else '')
+            # sorted_items.append(f"{item}\n" if 'Mythic' in item else '')
+            if 'Mythic' in item:
+                sorted_items.append(item)
         for item in items:
             print(item)
             if 'Mythic' not in item:

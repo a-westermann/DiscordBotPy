952b111f57afd637f58780c173ef90512bf2f029
Vierce1 <a.westermann.19@gmail.com>
2023-06-09 11:33:37 -0500

    establishing supabase connection

--- psql.py
+++ psql.py
@@ -48,7 +48,9 @@ class PSQL:
 
     def test_remote(self):
         return self.open_remote_connection(table_name='match_history')
-        json_response = requests.get(url=f"{db_url}/rest/v1/match_history", headers=headers).json()
+        params = {'query': "SELECT * FROM match_history"}
+        json_response = requests.get(url=f"{db_url}/rest/v1/match_history", headers=headers, params=params).json()
+        return json_response
 
 
 

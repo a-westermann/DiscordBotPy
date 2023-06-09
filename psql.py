f199a3d52865c50a80557884d18847aa97d94cf8
Vierce1 <a.westermann.19@gmail.com>
2023-06-09 09:57:58 -0500

    establishing supabase connection

--- psql.py
+++ psql.py
@@ -1,5 +1,6 @@
 import psycopg2
 import urllib3 as u3
+from requests.auth import HTTPBasicAuth as auths
 import json
 import cassiopeia
 
@@ -34,7 +35,11 @@ class PSQL:
         self.connection = psycopg2.connect(database=database, user=username, password=password,
                                             host=hostname, port=port, cursor_factory=RealDictCursor)
         self.cursor = self.connection.cursor()
-        response = http.request(method='GET', url=db_url)
+        header = {
+            'Accept' : 'application/json',
+            'Authorization' : remote_key
+        }
+        response = http.request(method='GET', url=db_url, headers=header)
         data = json.loads(response.data.decode('utf-8'))
         print(str(data))
 

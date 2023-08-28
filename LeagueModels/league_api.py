52320a61be8be584cfd9db56cfeab2d265b016f5
Vierce1 <a.westermann.19@gmail.com>
2023-08-28 12:12:15 -0500

    fix error kda deaths

--- LeagueModels/league_api.py
+++ LeagueModels/league_api.py
@@ -111,7 +111,6 @@ class LeagueAPI:
 
         # Totals stats
         kills, deaths, assists = match_rows[0]['kills'], match_rows[0]['deaths'], match_rows[0]['assists']
-        deaths = 1 if deaths == 0 else deaths
         kda = (float(kills) + float(assists))
         kda = round(kda, 2) if deaths == 0 else round(kda / float(deaths), 2)
         multi_kills = f"DTQP: {match_rows[0]['doubles']} / {match_rows[0]['triples']} / {match_rows[0]['quadras']}" \
@@ -126,7 +125,6 @@ class LeagueAPI:
         # Best game stats
         best_game = self.psql.get_best_match(summoner_name=summoner_name, champion=champ)
         kills, deaths, assists = best_game[0]['kills'], best_game[0]['deaths'], best_game[0]['assists']
-        deaths = 1 if deaths == 0 else deaths
         kda = (float(kills) + float(assists))
         kda = round(kda, 2) if deaths == 0 else round(kda / float(deaths), 2)
         multi_kills = f"DTQP: {best_game[0]['doubles']} / {best_game[0]['triples']} / {best_game[0]['quadras']}" \
